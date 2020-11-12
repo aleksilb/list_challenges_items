4e412920020c8649cf56be32df5b5819bb15f865
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2020-11-12 23:12:32 +0200

    Formatting and refactoring

--- api.py
+++ api.py
@@ -7,13 +7,16 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
 
+
 @app.route('/search/<search_string>', methods=['GET'])
 def search_items(search_string):
     return item_query("SELECT * FROM item WHERE name LIKE '%" + search_string + "%' AND lists > 0 ORDER BY lists DESC")
 
+
 @app.route('/top-items', methods=['GET'])
 def get_top_items():
     return item_query("SELECT * FROM item ORDER BY lists DESC LIMIT 100")
+
 
 def item_query(query):
     conn = sqlite3.connect('../Database/listchallenge.db')
@@ -21,7 +24,8 @@ def item_query(query):
     c.execute(query)
     items = []
     for result in c.fetchall():
-        items.append({"id":result[0],"name":result[1],"lists":result[2]})
+        items.append({"id": result[0], "name": result[1], "lists": result[2]})
     return jsonify(items)
+
 
 app.run()
--- screaper_async.py
+++ screaper_async.py
@@ -5,12 +5,14 @@ import sqlite3
 import asyncio
 from typing import NamedTuple
 
+
 class LcItem(NamedTuple):
     item_id: int
     name: str
     lists: int
 
-async def update_items(num:int):
+
+async def update_items(num: int):
     conn = sqlite3.connect('../Database/listchallenge.db')
     while True:
         next_id = get_last_id(conn) + 1
@@ -18,39 +20,48 @@ async def update_items(num:int):
             await asyncio.gather(*[update_item(item_id, conn, session) for item_id in range(next_id, next_id + num)])
     conn.close()
 
+
 async def update_item(item_id, conn, session):
     item = await get_item(item_id, session)
     print(item)
     save_item(item, conn)
     conn.commit()
 
+
 async def get_item(item_id, session):
     r = await session.get('https://www.listchallenges.com/lists/containing-item/' + str(item_id))
     item = parse_item(item_id, await r.text())
     return item
 
+
 def parse_item(item_id, content):
     soup = BeautifulSoup(content, 'html.parser')
     title = soup.title.string.strip()
-    name = title.replace('Lists containing','').replace('\'','').strip()
-    list_el = soup.find("div", string = re.compile("\d\slist"))
+    name = title.replace('Lists containing', '').replace('\'', '').strip()
+    list_el = soup.find("div", string=re.compile("\d\slist"))
     lists = '0'
     if list_el is not None:
-        lists = list_el.string.replace('lists','').replace('list','').strip()
+        lists = list_el.string.replace('lists', '').replace('list', '').strip()
         if "of" in lists:
             lists = lists.split("of")[1].strip()
-        lists = lists.replace(',','')
+        lists = lists.replace(',', '')
     return LcItem(item_id, name, lists)
 
-def save_item(item:LcItem, conn):
+
+def query_db(query, conn):
     c = conn.cursor()
-    query = "INSERT INTO item VALUES ("+ str(item.item_id) +",'"+item.name+"',"+str(item.lists)+")"/
     c.execute(query)
+    return c
+
+
+def save_item(item: LcItem, conn):
+    query_db("INSERT INTO item VALUES (" + str(item.item_id) + ",'" + item.name + "'," + str(item.lists) + ")", conn)
+
 
 def get_last_id(conn):
-    c = conn.cursor()
-    c.execute("SELECT MAX(id) FROM item")
+    c = query_db("SELECT MAX(id) FROM item", conn)
     return c.fetchone()[0]
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(update_items(5))
