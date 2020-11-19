8652d33b6804bc63bab5e848ab6a2980b9b687f3
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2020-11-19 23:49:51 +0200

    Only show unchecked top items

--- server/api.py
+++ server/api.py
@@ -16,7 +16,7 @@ def search_items(search_string):
 
 @app.route('/top-items', methods=['GET'])
 def get_top_items():
-    return do_item_query("SELECT * FROM item ORDER BY lists DESC LIMIT 100")
+    return do_item_query("SELECT * FROM item WHERE checked = FALSE ORDER BY lists DESC LIMIT 100")
 
 
 @app.route('/items/<item_id>/check', methods=['PUT'])
