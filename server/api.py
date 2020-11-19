298639e9a9e249c6ad10c55eec7e9b106b3e13e1
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2020-11-19 23:48:34 +0200

    Api end point for checking a list item

--- server/api.py
+++ server/api.py
@@ -11,22 +11,36 @@ CORS(app)
 
 @app.route('/search/<search_string>', methods=['GET'])
 def search_items(search_string):
-    return item_query("SELECT * FROM item WHERE name LIKE '%" + search_string + "%' AND lists > 0 ORDER BY lists DESC")
+    return do_item_query("SELECT * FROM item WHERE name LIKE '%" + search_string + "%' AND lists > 0 ORDER BY lists DESC")
 
 
 @app.route('/top-items', methods=['GET'])
 def get_top_items():
-    return item_query("SELECT * FROM item ORDER BY lists DESC LIMIT 100")
+    return do_item_query("SELECT * FROM item ORDER BY lists DESC LIMIT 100")
 
 
-def item_query(query):
-    conn = sqlite3.connect(os.path.join(current_dir, 'listchallenge.db'))
-    c = conn.cursor()
-    c.execute(query)
+@app.route('/items/<item_id>/check', methods=['PUT'])
+def check_item(item_id):
+    do_query("UPDATE item SET checked = TRUE WHERE id = " + item_id)
+    return "{}"
+
+
+def do_item_query(query):
+    results = do_query(query)
     items = []
-    for result in c.fetchall():
+    for result in results:
         items.append({"id": result[0], "name": result[1], "lists": result[2]})
     return jsonify(items)
+
+
+def do_query(query):
+    connection = sqlite3.connect(os.path.join(current_dir, 'listchallenge.db'))
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return results
 
 
 if __name__ == "__main__":
